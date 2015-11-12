ca965fe6c31b8446acb5b5a32d8750cbcb5a82d3
Albarita <albaramirez12345@gmail.com>
2015-11-11 23:40:22 -0600

    programacion

---
+++ RANGOFOR.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int n1;
+int n2;
+int i;
+int main()
+{
+ printf(" INGRESA INICIO\n");
+ scanf("  \n %d",&n1);
+ printf(" INGRESA FINAL\n");
+ scanf("  \n %d",&n2);
+ 
+        for (i=n1; i<=n2;  i++);	{
+             printf("\n%d",&i);
+
+                                    }	
+	system("pause");
+}
